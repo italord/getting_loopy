4ec15d45d04d039165180c038c03d361868f11b2
italo robinson <italord@italos-MacBook-Pro.local>
2016-03-08 21:41:59 -1000

    even Arrays

--- loop.js
+++ loop.js
@@ -6,3 +6,22 @@ console.log ("The value of:" + [i]);
 console.log ("The value at index is:" + Presidents [i]);
 }
 
+// `The String of Numbers`
+
+var stringOfNumbers = "";
+
+for (var i = 10; i <= 20; i++){
+	stringOfNumbers = stringOfNumbers + i;
+} 
+console.log (stringOfNumbers);
+
+// `Add only even numbrs to an array`
+
+var evenNumberArray = [];
+
+for (var i = 0; i <= 50; i+= 2){
+	evenNumberArray.push(i); 
+}
+
+console.log (evenNumberArray);
+
