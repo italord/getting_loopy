1924287563b83ea101e9810a825e4879eb1041a7
italo robinson <italord@italos-MacBook-Pro.local>
2016-03-08 20:53:35 -1000

    deadpresidents

--- loop.js
+++ loop.js
@@ -1,5 +1,7 @@
 
+var Presidents = ["Washington", "Adams", "Jefferson", "Madison", "Monroe"];
 
-Function Presidents (){
-
+for (var i = 0; i <Presidents.length; i++) {
+console.log ("The value of:" + [i]);
+console.log ("The value at index is:" + Presidents [i]);
 }
